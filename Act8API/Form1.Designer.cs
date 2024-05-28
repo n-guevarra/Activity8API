195b7554d2bc1f34283ad33cd95b2d92a358d7fd
n-guevarra <168363742+n-guevarra@users.noreply.github.com>
2024-05-29 01:56:37 +0800

    Add files via upload

---
+++ Act8API/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Act8API
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lnametb = new System.Windows.Forms.TextBox();
+            this.fnametb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.doctortb = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.datetb = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.postBtn = new System.Windows.Forms.Button();
+            this.getBtn = new System.Windows.Forms.Button();
+            this.cardioBtn = new System.Windows.Forms.RadioButton();
+            this.hemeBtn = new System.Windows.Forms.RadioButton();
+            this.pulmoBtn = new System.Windows.Forms.RadioButton();
+            this.radioBtns = new System.Windows.Forms.GroupBox();
+            this.gunaDataGridView1 = new Guna.UI.WinForms.GunaDataGridView();
+            this.radioBtns.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Activity 8: API";
+            // 
+            // lnametb
+            // 
+            this.lnametb.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnametb.Location = new System.Drawing.Point(356, 256);
+            this.lnametb.Name = "lnametb";
+            this.lnametb.Size = new System.Drawing.Size(186, 27);
+            this.lnametb.TabIndex = 2;
+            // 
+            // fnametb
+            // 
+            this.fnametb.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fnametb.Location = new System.Drawing.Point(164, 256);
+            this.fnametb.Name = "fnametb";
+            this.fnametb.Size = new System.Drawing.Size(186, 27);
+            this.fnametb.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(161, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(353, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Last Name";
+            // 
+            // doctortb
+            // 
+            this.doctortb.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.doctortb.Location = new System.Drawing.Point(356, 305);
+            this.doctortb.Name = "doctortb";
+            this.doctortb.Size = new System.Drawing.Size(186, 27);
+            this.doctortb.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(353, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Doctor Assigned";
+            // 
+            // datetb
+            // 
+            this.datetb.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datetb.Location = new System.Drawing.Point(356, 354);
+            this.datetb.Name = "datetb";
+            this.datetb.Size = new System.Drawing.Size(186, 27);
+            this.datetb.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(353, 335);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Available Date";
+            // 
+            // postBtn
+            // 
+            this.postBtn.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postBtn.Location = new System.Drawing.Point(560, 237);
+            this.postBtn.Name = "postBtn";
+            this.postBtn.Size = new System.Drawing.Size(137, 143);
+            this.postBtn.TabIndex = 15;
+            this.postBtn.Text = "POST";
+            this.postBtn.UseVisualStyleBackColor = true;
+            this.postBtn.Click += new System.EventHandler(this.postBtn_Click);
+            // 
+            // getBtn
+            // 
+            this.getBtn.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getBtn.Location = new System.Drawing.Point(13, 237);
+            this.getBtn.Name = "getBtn";
+            this.getBtn.Size = new System.Drawing.Size(137, 143);
+            this.getBtn.TabIndex = 16;
+            this.getBtn.Text = "GET";
+            this.getBtn.UseVisualStyleBackColor = true;
+            this.getBtn.Click += new System.EventHandler(this.getBtn_Click);
+            // 
+            // cardioBtn
+            // 
+            this.cardioBtn.AutoSize = true;
+            this.cardioBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardioBtn.Location = new System.Drawing.Point(6, 23);
+            this.cardioBtn.Name = "cardioBtn";
+            this.cardioBtn.Size = new System.Drawing.Size(90, 21);
+            this.cardioBtn.TabIndex = 19;
+            this.cardioBtn.TabStop = true;
+            this.cardioBtn.Text = "Cardiology";
+            this.cardioBtn.UseVisualStyleBackColor = true;
+            this.cardioBtn.CheckedChanged += new System.EventHandler(this.cardioBtn_CheckedChanged);
+            // 
+            // hemeBtn
+            // 
+            this.hemeBtn.AutoSize = true;
+            this.hemeBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hemeBtn.Location = new System.Drawing.Point(6, 45);
+            this.hemeBtn.Name = "hemeBtn";
+            this.hemeBtn.Size = new System.Drawing.Size(97, 21);
+            this.hemeBtn.TabIndex = 20;
+            this.hemeBtn.TabStop = true;
+            this.hemeBtn.Text = "Hematology";
+            this.hemeBtn.UseVisualStyleBackColor = true;
+            this.hemeBtn.CheckedChanged += new System.EventHandler(this.hemeBtn_CheckedChanged);
+            // 
+            // pulmoBtn
+            // 
+            this.pulmoBtn.AutoSize = true;
+            this.pulmoBtn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pulmoBtn.Location = new System.Drawing.Point(6, 67);
+            this.pulmoBtn.Name = "pulmoBtn";
+            this.pulmoBtn.Size = new System.Drawing.Size(102, 21);
+            this.pulmoBtn.TabIndex = 21;
+            this.pulmoBtn.TabStop = true;
+            this.pulmoBtn.Text = "Pulmonology";
+            this.pulmoBtn.UseVisualStyleBackColor = true;
+            this.pulmoBtn.CheckedChanged += new System.EventHandler(this.pulmoBtn_CheckedChanged);
+            // 
+            // radioBtns
+            // 
+            this.radioBtns.Controls.Add(this.cardioBtn);
+            this.radioBtns.Controls.Add(this.pulmoBtn);
+            this.radioBtns.Controls.Add(this.hemeBtn);
+            this.radioBtns.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtns.Location = new System.Drawing.Point(164, 289);
+            this.radioBtns.Name = "radioBtns";
+            this.radioBtns.Size = new System.Drawing.Size(186, 92);
+            this.radioBtns.TabIndex = 22;
+            this.radioBtns.TabStop = false;
+            this.radioBtns.Text = "Department to Visit";
+            // 
+            // gunaDataGridView1
+            // 
+            this.gunaDataGridView1.AllowUserToResizeColumns = false;
+            this.gunaDataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.gunaDataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.gunaDataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gunaDataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.gunaDataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gunaDataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.gunaDataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gunaDataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.gunaDataGridView1.ColumnHeadersHeight = 22;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gunaDataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.gunaDataGridView1.EnableHeadersVisualStyles = false;
+            this.gunaDataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gunaDataGridView1.Location = new System.Drawing.Point(13, 32);
+            this.gunaDataGridView1.Name = "gunaDataGridView1";
+            this.gunaDataGridView1.ReadOnly = true;
+            this.gunaDataGridView1.RowHeadersVisible = false;
+            this.gunaDataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gunaDataGridView1.Size = new System.Drawing.Size(684, 191);
+            this.gunaDataGridView1.TabIndex = 18;
+            this.gunaDataGridView1.Theme = Guna.UI.WinForms.GunaDataGridViewPresetThemes.Guna;
+            this.gunaDataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.gunaDataGridView1.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.gunaDataGridView1.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.gunaDataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.gunaDataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.gunaDataGridView1.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.gunaDataGridView1.ThemeStyle.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gunaDataGridView1.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.SteelBlue;
+            this.gunaDataGridView1.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.gunaDataGridView1.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaDataGridView1.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.gunaDataGridView1.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.gunaDataGridView1.ThemeStyle.HeaderStyle.Height = 22;
+            this.gunaDataGridView1.ThemeStyle.ReadOnly = true;
+            this.gunaDataGridView1.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.gunaDataGridView1.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.gunaDataGridView1.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaDataGridView1.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.gunaDataGridView1.ThemeStyle.RowsStyle.Height = 22;
+            this.gunaDataGridView1.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gunaDataGridView1.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(709, 393);
+            this.Controls.Add(this.radioBtns);
+            this.Controls.Add(this.gunaDataGridView1);
+            this.Controls.Add(this.getBtn);
+            this.Controls.Add(this.postBtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.datetb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.doctortb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fnametb);
+            this.Controls.Add(this.lnametb);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.radioBtns.ResumeLayout(false);
+            this.radioBtns.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lnametb;
+        private System.Windows.Forms.TextBox fnametb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox doctortb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox datetb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button postBtn;
+        private System.Windows.Forms.Button getBtn;
+        private System.Windows.Forms.RadioButton cardioBtn;
+        private System.Windows.Forms.RadioButton hemeBtn;
+        private System.Windows.Forms.RadioButton pulmoBtn;
+        private System.Windows.Forms.GroupBox radioBtns;
+        private Guna.UI.WinForms.GunaDataGridView gunaDataGridView1;
+    }
+}
+
---
+++ Act8API/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+
+namespace Act8API
+{
+    public partial class Form1 : Form
+    {
+        private readonly string apiUrl = "http://localhost/myapi/act8api.php";
+        private string selectedDepartment = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void cardioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            selectedDepartment = "Cardiology";
+            await FetchDoctorAndAvailableDate();
+        }
+
+        private async void hemeBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            selectedDepartment = "Hematology";
+            await FetchDoctorAndAvailableDate();
+        }
+
+        private async void pulmoBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            selectedDepartment = "Pulmonology";
+            await FetchDoctorAndAvailableDate();
+        }
+
+        private async Task FetchDoctorAndAvailableDate()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync($"{apiUrl}?department={selectedDepartment}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    var data = JObject.Parse(json);
+
+                    if (data["error"] == null)
+                    {
+                        doctortb.Text = data["doctor"].ToString(); 
+                        datetb.Text = data["available_date"].ToString(); 
+                        await FetchDoctorAndAvailableDate();
+;                       
+                    }
+                    else
+                    {
+                        MessageBox.Show(data["error"].ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to retrieve data from the server. Status code: " + response.StatusCode);
+                }
+            }
+        }
+
+
+
+        private async void getBtn_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    var data = JArray.Parse(json);
+                    gunaDataGridView1.DataSource = data.ToObject<DataTable>();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to retrieve data from the server. Status code: " + response.StatusCode);
+                }
+            }
+        }
+
+
+        private async void postBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(selectedDepartment))
+            {
+                MessageBox.Show("Please select a department.");
+                return;
+            }
+
+            var patientData = new
+            {
+                first_name = fnametb.Text,
+                last_name = lnametb.Text,
+                department = selectedDepartment,
+                doctor = doctortb.Text,
+                available_date = datetb.Text
+            };
+
+            string json = JsonConvert.SerializeObject(patientData);
+
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show(responseBody);
+                }
+                else
+                {
+                    MessageBox.Show("Failed to post data to the server. Status code: " + response.StatusCode);
+                }
+            }
+        }
+ 
+    }
+}
